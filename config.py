d7096a4be023ad5f65b5e8553981e34f2687214d
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-22 10:49:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,117 +1,34 @@
-import os
-from instaloader import Instaloader
-from dotenv import load_dotenv
+import random
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-load_dotenv()
+# Replace with your actual bot token
+TOKEN = '6416227615:AAG6WpsPt0GgZzzlxIFOj2m9T0aBZ0NTb0I'
 
-class Config:
-    API_ID = int(os.environ.get("API_ID", "22894281"))
-    API_HASH = os.environ.get("API_HASH", "b59176de6ec96903b816d72c2ad4fff8")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6157850456:AAECSQKBOrjcmF_mI9ngnltvsHqjV7TBOEk") 
-    USER = os.environ.get("INSTAGRAM_USERNAME", "simple_life_is_great")
-    OWNER = os.environ.get("OWNER_ID", "5841005593")
-    INSTA_SESSIONFILE_ID = os.environ.get("INSTA_SESSIONFILE_ID", None)
-    S = "0"
-    STATUS = set(int(x) for x in (S).split())
-    L=Instaloader()
-    HELP="""
-You can Download almost anything From your Instagram Account.
+# List of video URLs
+video_urls = [
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164269845889>
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164241156841>
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164052413165>
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164246101925>
+]
 
-<b>What Can Be Downloaded?:</b>
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Send /reel to get a random video!")
 
-1. All posts of any Profile. (Both Public and Private,for private profiles you need to be a follower.)
-2. All Posts from your feed.
-3. Stories of any profile (Both Public and Private,for private profiles you need to be a follower.)
-4. DP of any profile (No need to follow)
-5. Followers and Followees List of any Profile.
-6. List of followees who follows back the given username.
-7. List of followees who are not following back the given username.
-8. Stories of your Followees.
-9. Tagged posts of any profile.
-10. Your saved Posts.
-11. IGTV videos.
-12. Highlights from any profiles.
-13. Any Public Post from Link(Post/Reels/IGTV)
+def reel(update: Update, context: CallbackContext) -> None:
+    random_video_url = random.choice(video_urls)
+    update.message.reply_video(random_video_url)
 
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-<b>How to Download:</b>
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("reel", reel))
 
-Its Easy!!
-You Need to login into your account by /login. 
+    updater.start_polling()
+    updater.idle()
 
-You have two Options:
-
-1. From Username:
-
-Just send any instagram username.
-
-For Example:
-<code>samantharuthprabhuoffl</code>
-<code>subin_p_s_</code>
-<code>_chill_manh_7</code>
-
-
-2. From URL:
-
-You can also sent a post link to download the post or video.
-
-For Example:
-<code>https://www.instagram.com/p/CL4QbUiLRNW/?utm_medium=copy_link</code>
-<code>https://www.instagram.com/taylorswift/p/CWds7zapgHn/?utm_medium=copy_link</code>
-
-
-<b>Available Commands and Usage</b>
-
-/start - Check wheather bot alive.
-/restart - Restart the bot (If you messed up anything use /restart.)
-/help - Shows this menu.
-/login - Login into your account.
-/logout - Logout of your account.
-/account - Shows the details of logged in account.
-
-/posts <username> - Download posts of any username. Use /posts to download own posts or <code> /posts <username> </code>for others.
-Example : <code>/posts samantharuthprabhuoffl</code>
-
-/igtv <username> - Download IGTV videos from given username. If no username given, downloads your IGTV.
-
-/feed <number of posts to download> - Downloads posts from your feed.If no number specified all posts from feed will be downloaded.
-Example: <code>/feed 10</code> to download latest 10 posts from feed.
-
-/saved <number of posts to download> - Downloads your saved posts. If no number specified all saved posts will be downloaded.
-Example: <code>/saved 10</code> to download latest 10 saved posts.
-
-/followers <username> - Get a list of all followers of given username. If no username given, then your list will be retrieved.
-Example: <code>/followers samantharuthprabhuoffl</code>
-
-/followees <username> - Get a list of all followees of given username. If no username given, then your list will be retrieved.
-
-/fans <username> - Get a list of of followees who follow back the given username. If no username given, your list will be retrieved.
-
-/notfollowing <username> - Get a list of followees who is not following back the given username.
-
-/tagged <username> - Downloads all posts in which given username is tagged. If nothing given your tagged posts will be downloaded.
-
-/story <username> - Downloads all stories from given username. If nothing given your stories will be downloaded.
-
-/stories - Downloads all the stories of all your followees.
-
-/highlights <username> - Downloads highlights from given username, If nothing given your highlights will be downloaded.
-
-
-"""
-    HOME_TEXT = """
-<b>Helo, [{}](tg://user?id={})
-
-This is a bot of [{}](www.instagram.com/{}) to manage his Instagram account. 
-I can only work for my master [{}](tg://user?id={}).
-But you can Deploy the same bot for your use from the below source code.
-
-Use /help to know What I can Do?</b>
-"""
-    HOME_TEXT_OWNER = """
-<b>Helo, [{}](tg://user?id={})
-I am your assistant to manage your Instagram account.
-
-Use /help to know what I can do for you.</b>
-"""
-
+if __name__ == '__main__':
+    main()
