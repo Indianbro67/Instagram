900df6f604e6347b43978ec48000993e94ab0cc0
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-22 11:02:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,34 @@
 import random
+import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # Replace with your actual bot token
-TOKEN = '6416227615:AAG6WpsPt0GgZzzlxIFOj2m9T0aBZ0NTb0I'
+TOKEN = '6028447687:AAGJtVpsUDL6KAzDzcm6LPT5nYN9rjQwTuQ'
 
 # List of video URLs
 video_urls = [
-    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164269845889>
-    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164241156841>
-    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164052413165>
-    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164246101925>
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164269845889",
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164241156841",
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164052413165",
+    "https://cdn.discordapp.com/attachments/1101163334692245546/1101164246101925",
 ]
+
+# List of allowed video extensions
+allowed_extensions = ['.mp4', '.mov']
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Send /reel to get a random video!")
 
 def reel(update: Update, context: CallbackContext) -> None:
-    random_video_url = random.choice(video_urls)
-    update.message.reply_video(random_video_url)
+    # Filter video URLs by allowed extensions
+    valid_video_urls = [url for url in video_urls if any(url.endswith(ext) for ext in allowed_extensions)]
+    
+    if valid_video_urls:
+        random_video_url = random.choice(valid_video_urls)
+        update.message.reply_video(random_video_url)
+    else:
+        update.message.reply_text("No valid videos available.")
 
 def main():
     updater = Updater(token=TOKEN, use_context=True)
